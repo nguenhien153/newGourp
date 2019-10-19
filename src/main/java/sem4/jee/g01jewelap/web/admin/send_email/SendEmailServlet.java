45075e7010403efefc3c4f8a7dfa5294f6cf57bc
viet <viet@Ducviet>
2019-10-19 10:39:10 +0700

    edit jewelAp

---
+++ src/main/java/sem4/jee/g01jewelap/web/admin/send_email/SendEmailServlet.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sem4.jee.g01jewelap.web.admin.send_email;
+
+/**
+ *
+ * @author viet
+ */
+public class SendEmailServlet {
+    
+}
